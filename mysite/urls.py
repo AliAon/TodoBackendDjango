65c5bcf0278a5f4635b18d602aa13f13bc48c9a9
aon <aon094944@gmail.com>
2022-12-29 12:36:55 +0500

    todo backend

---
+++ mysite/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from app import views
+router=DefaultRouter()
+router.register(r'todo', views.TodoViewset,basename="todo")
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/',include(router.urls))
+]
